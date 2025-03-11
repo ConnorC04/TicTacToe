57c56be93df733d591a384dd1e216b769866a317
ConnorC04 <connorjcasey@iCloud.com>
2025-03-11 15:30:02 -0400

    holy sh*t I did it

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -5,37 +5,100 @@ package rocks.zipcodewilmington.tictactoe;
  */
 public class Board {
 
-    public int xInRow = 0;
-    public int oInRow = 0;
-    public int xInColumn = 0;
-    public int oInColumn = 0;
-    public int xInDiagonal = 0;
-    public int oInDiagonal = 0;
+    public int xCounter = 0;
+    public int oCounter = 0;
+
 
     public Board(Character[][] matrix) {
         // 2 dimensional Character Array "matrix"
         char x = 'X';
         char o = 'O';
 
+        int i; // Row Number
+        int j; // Column Number
 
-        // Iterate through rows
-        for (int i = 0; i < matrix.length; i++){
-            for (int j = 0; j < matrix.length; j++){
-                if (matrix[i][j] == x){
-                    xInRow++;
-                } else if (matrix[i][j] == o) {
-                    oInRow++;
+
+        // Iterate through rows left to right, checks for a sequence of 3 x's or o's in a row, if true, break.
+            for (i = 0; i < matrix.length; i++) {
+                for (j = 0; j < matrix.length; j++) {
+                    if (matrix[i][j] == x) {
+                        xCounter++;
+                    } else if (matrix[i][j] == o) {
+                        oCounter++;
+                    }
+                }
+                if (xCounter == 3 || oCounter == 3) {
+                    break;
+                } else {
+                    xCounter = 0;
+                    oCounter = 0;
+                }
+            }
+
+        if (xCounter == 0 && oCounter == 0) {
+            for (i = 0; i < matrix.length; i++) {
+                for (j = 0; j < matrix.length; j++) {
+                    if (matrix[j][i] == x) {
+                        xCounter++;
+                    } else if (matrix[j][i] == o) {
+                        oCounter++;
+                    }
+                }
+                if (xCounter == 3 || oCounter == 3) {
+                    break;
+                } else {
+                    xCounter = 0;
+                    oCounter = 0;
+                }
+            }
+        }
+
+        if (xCounter == 0 && oCounter == 0) {
+            for (i = 0; i < matrix.length; i++) {
+                for (j = 0; j < matrix.length; j++) {
+                    if (matrix[j][j] == x) {
+                        xCounter++;
+                    } else if (matrix[j][j] == o) {
+                        oCounter++;
+                    }
+                }
+                if (xCounter == 3 || oCounter == 3) {
+                    break;
+                } else {
+                    xCounter = 0;
+                    oCounter = 0;
+                }
+            }
+        }
+
+        if (xCounter == 0 && oCounter == 0){
+            for (i = 2; i >= 0; i--) {
+                for (j = 0; j < matrix.length; j++) {
+                    if (matrix[i][j] == x) {
+                        xCounter++;
+                    } else if (matrix[i][j] == o) {
+                        oCounter++;
+                    }
+                    i--;
+                }
+                if (xCounter == 3 || oCounter == 3) {
+                    break;
+                } else {
+                    xCounter = 0;
+                    oCounter = 0;
                 }
             }
         }
     }
 
+
+
     public Boolean isInFavorOfX() {
-        return (xInRow == 3 || xInColumn == 3 || xInDiagonal == 3);
+        return xCounter == 3;
     }
 
     public Boolean isInFavorOfO() {
-        return (oInRow == 3 || oInColumn == 3 || oInDiagonal == 3);
+        return oCounter == 3;
     }
 
     public Boolean isTie() {
@@ -44,7 +107,14 @@ public class Board {
 
     public String getWinner() {
 
-        return null;
+        if (isInFavorOfX()){
+            return "X";
+        } else if (isInFavorOfO()) {
+            return  "O";
+        } else if (isTie()) {
+            return "";
+        }
+        return "Unreachable statement";
     }
 
 }
