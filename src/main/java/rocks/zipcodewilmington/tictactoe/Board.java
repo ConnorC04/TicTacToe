5dc70f0adaa3199f7359e130b06c81ec8d482d15
ConnorC04 <connorjcasey@iCloud.com>
2025-03-11 13:03:37 -0400

    Checkpoint

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,22 +4,46 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    public int xInRow = 0;
+    public int oInRow = 0;
+    public int xInColumn = 0;
+    public int oInColumn = 0;
+    public int xInDiagonal = 0;
+    public int oInDiagonal = 0;
+
     public Board(Character[][] matrix) {
+        // 2 dimensional Character Array "matrix"
+        char x = 'X';
+        char o = 'O';
+
+
+        // Iterate through rows
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix.length; j++){
+                if (matrix[i][j] == x){
+                    xInRow++;
+                } else if (matrix[i][j] == o) {
+                    oInRow++;
+                }
+            }
+        }
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return (xInRow == 3 || xInColumn == 3 || xInDiagonal == 3);
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return (oInRow == 3 || oInColumn == 3 || oInDiagonal == 3);
     }
 
     public Boolean isTie() {
-        return null;
+        return (!isInFavorOfO() && !isInFavorOfX());
     }
 
     public String getWinner() {
+
         return null;
     }
 
